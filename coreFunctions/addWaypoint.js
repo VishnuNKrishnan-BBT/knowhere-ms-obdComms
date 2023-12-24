d0eed4ba0527a1d13a42377cd8c03c2285537951
VishnuNKrishnan <navaneeth.vishnu1@gmail.com>
2023-12-24 20:29:02 +0400

    Undefined fields added

--- coreFunctions/addWaypoint.js
+++ coreFunctions/addWaypoint.js
@@ -15,6 +15,17 @@ const addWaypoint = ({
 
     console.log('Adding new waypoint...');
 
+    console.log(
+        trackerId,
+        timestamp,
+        latitude,
+        longitude,
+        heading,
+        speed,
+        altitude,
+        accuracy
+    );
+
     var actionBlocked = false
 
     //Block if trackerId does not exist in trackers collection
@@ -30,23 +41,39 @@ const addWaypoint = ({
             })
             return //Abort operation if action is blocked
         } else { //Continue operation if action is not blocked
-            const Waypoint = createWaypointModel(trackerId)
-            const newWaypoint = Waypoint({
-                timestamp: timestamp,
-                latitude: latitude,
-                longitude: longitude,
-                heading: heading,
-                speed: speed,
-                altitude: altitude,
-                accuracy: accuracy
-            })
 
-            newWaypoint.save()
-            res.json({
-                status: 200,
-                timestamp: timestamp,
-                message: `Waypoint added successfully!`
-            })
+            if (
+                trackerId === undefined ||
+                timestamp === undefined ||
+                latitude === undefined ||
+                longitude === undefined ||
+                heading === undefined ||
+                speed === undefined
+            ) {
+                res.json({
+                    status: 500,
+                    timestamp: timestamp,
+                    message: `Ignoring request due to undefined fields.`
+                })
+            } else {
+                const Waypoint = createWaypointModel(trackerId)
+                const newWaypoint = Waypoint({
+                    timestamp: timestamp,
+                    latitude: latitude,
+                    longitude: longitude,
+                    heading: heading,
+                    speed: speed,
+                    altitude: altitude,
+                    accuracy: accuracy
+                })
+
+                newWaypoint.save()
+                res.json({
+                    status: 200,
+                    timestamp: timestamp,
+                    message: `Waypoint added successfully!`
+                })
+            }
         }
     })
 }
